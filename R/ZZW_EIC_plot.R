871472a1fb6c06eaaf999299dbe0e46f7a9b6b86
Zhou Zhiwei <Zhou Zhiwei>
2017-10-18 16:00:57 +0800

    update axis plot

--- R/ZZW_EIC_plot.R
+++ R/ZZW_EIC_plot.R
@@ -27,7 +27,19 @@ ZZW_EIC_plot <- function(raw.data,
 
   pdf(file = pic.name, width = 6, height = 4)
 
-  plot(as.numeric(raw.data$Time), as.numeric(raw.data$Counts)/max.count, type = "l", lwd=2, xlab = "Retention time (s)", ylab = "Counts", main = "Extract Ion Chromatography", col=color, bty="n", rt.range)
+  plot(as.numeric(raw.data$Time),
+       as.numeric(raw.data$Counts)/max.count, type = "l", lwd=2, xlab = "Retention time (s)",
+       ylab = "Counts",
+       main = "Extract Ion Chromatography",
+       col=color,
+       bty="n",
+       xlim = rt.range,
+       axes = F)
+
+  axis(side = 1, at=seq(round(rt.range[1], digits = 0), round(rt.range[2], digits = 0), length.out = 5), labels = seq(round(rt.range[1], digits = 0), round(rt.range[2], digits = 0), length.out = 5))
+
+  axis(side = 2, at=c(0,0.5,1), labels = c(0, 0.5, 1))
+
   box()
 
   dev.off()
