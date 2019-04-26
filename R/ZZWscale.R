224f9ca50bb1a69c364015191beb8bab91e530ea
JustinZZW <zhiwei92@163.com>
2019-04-26 21:17:21 +0800

    add a function of ZZWscale

--- R/ZZWscale.R
+++ R/ZZWscale.R
@@ -37,3 +37,64 @@ ZZWscale <- function(raw.data, method = "auto"){
   }
   return(temp.result)
 }
+
+
+#' @title ZZWzcore
+#' @author Zhiwei Zhou
+#' @param raw_data a matrix or data.frame in numeric
+#' @param type "by_column" or "by_row". Default: "by_row"
+#' @export
+
+setGeneric(name = 'ZZWzscore',
+           def = function(raw_data,
+                          type=c('by_row', 'by_column')){
+
+             type <- match.arg(type)
+             method <- match.arg(method)
+
+             if (type=='by_column') {
+               temp_mean <- apply(raw_data, 2, mean)
+               temp_sd <- apply(raw_data, 2, sd)
+               standardization_table <- data.frame(mean=temp_mean,
+                                                   sd=temp_sd,
+                                                   stringsAsFactors = F)
+
+               stanardization_result <- apply(raw_data, 2, function(x){
+                 temp <- as.numeric(x)
+                 temp_mean <- mean(x)
+                 temp_sd <- sd(x)
+                 temp_result <- (x-temp_mean)/temp_sd
+                 temp_result
+               })
+
+               result <- list(stanardization_result=stanardization_result,
+                              standardization_table=standardization_table)
+
+               return(result)
+
+             }
+
+             if (type=='by_row') {
+               temp_mean <- apply(raw_data, 1, mean)
+               temp_sd <- apply(raw_data, 1, sd)
+               standardization_table <- data.frame(mean=temp_mean,
+                                                   sd=temp_sd,
+                                                   stringsAsFactors = F)
+
+               stanardization_result <- apply(raw_data, 1, function(x){
+                 temp <- as.numeric(x)
+                 temp_mean <- mean(x)
+                 temp_sd <- sd(x)
+                 temp_result <- (x-temp_mean)/temp_sd
+                 temp_result
+               })
+
+               result <- list(stanardization_result=stanardization_result,
+                              standardization_table=standardization_table)
+
+               return(result)
+
+             }
+
+           }
+)
